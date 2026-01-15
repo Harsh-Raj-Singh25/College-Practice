5bbea1cadb69fcad1780c84f31c33989eba8bed4
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2026-01-15 15:27:07 +0530

    [Harsh Raj Singh] : added mini project-3

--- Assignments/Projects/src/Mini_Project_3/StudentValidationException.java
+++ Assignments/Projects/src/Mini_Project_3/StudentValidationException.java
@@ -1,5 +1,21 @@
 package Mini_Project_3;
+/*
+ * Mini Project 3
+Add login system (username/password)
+1. Add Students
+2. Display All Students
+3. Search Students by Eno
+4. Update Students Branch
+5. Delete Students by Eno
+6. Display Sorted Students
+7. Exit
 
+Validation Rules
+	Students Eno must be unique
+	Percentage must be positive
+	Sem and branch cannot be empty
+Exception handling with custom messages
+ */
 //Custom Exception for validation failures
 class StudentValidationException extends Exception {
 	public StudentValidationException(String message) {
